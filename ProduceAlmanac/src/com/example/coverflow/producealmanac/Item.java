9b06f6c2c8da42e0276ffa34c2810889816d5ba0
Sang <Sang@Sang.local>
2013-04-07 15:17:35 -0700

    added Item.java

---
+++ ProduceAlmanac/src/com/example/coverflow/producealmanac/Item.java
@@ -0,0 +1,30 @@
+package com.example.coverflow.producealmanac;
+
+import java.util.HashMap;
+
+import android.graphics.Bitmap;
+
+public class Item {
+	
+	static HashMap<String,Object[]> infoMap = new HashMap<String, Object[]>();
+	
+	String name;
+	String description;
+	String storage;
+	Bitmap picture;
+	/*
+	HOW TO LOAD A PICTURE FROM THE DRAWABLE FOLDER
+	Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.name_of_file);
+	someImageView.setImageBitmap(bitmap);
+	*/
+	
+	public Item(String name){
+		this.name = name;
+		this.description = (String) infoMap.get(name)[0];
+		this.storage = (String) infoMap.get(name)[1];
+		this.picture = (Bitmap) infoMap.get(name)[2];
+	}
+	
+
+		
+}
