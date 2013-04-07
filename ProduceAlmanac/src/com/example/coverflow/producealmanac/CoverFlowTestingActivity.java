35e85ad36c870a2a87816f7781f8c7c621660257
Sang <Sang@Sang.local>
2013-04-07 15:24:29 -0700

    added Item stuff in main activity

--- ProduceAlmanac/src/com/example/coverflow/producealmanac/CoverFlowTestingActivity.java
+++ ProduceAlmanac/src/com/example/coverflow/producealmanac/CoverFlowTestingActivity.java
@@ -1,16 +1,22 @@
 package com.example.coverflow.producealmanac;
 
+import java.util.ArrayList;
+
 import com.example.coverflow.CoverFlow;
 import com.example.coverflow.R;
 import com.example.coverflow.ReflectingImageAdapter;
 import com.example.coverflow.ResourceImageAdapter;
+import com.example.coverflow.producealmanac.Item;
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 /**
@@ -18,7 +24,10 @@ import android.widget.TextView;
  */
 public class CoverFlowTestingActivity extends Activity {
 
-    private TextView textView;
+	//currentItems will be the items currently in season, use a hashmap with key=time of year
+	ArrayList<Item> currentItems;
+	boolean populated=false;
+	TextView textView;
 
     /*
      * (non-Javadoc)
@@ -27,7 +36,27 @@ public class CoverFlowTestingActivity extends Activity {
      */
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+
+    	super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+		if(!populated){
+			populated=true;
+			populateMap();
+		}
+		
+		//add all current items to the ArrayList
+		currentItems = new ArrayList<Item>();
+		currentItems.add(new Item("apple"));
+		currentItems.add(new Item("potato"));
+		currentItems.add(new Item("strawberries"));
+		//etc etc
+		
+
+		//CoverFlow code below
+
+    	
+//        super.onCreate(savedInstanceState);
 
         setContentView(R.layout.main);
         textView = (TextView) findViewById(this.getResources()
@@ -85,5 +114,27 @@ public class CoverFlowTestingActivity extends Activity {
             }
         });
     }
+    
+	public void populateMap(){
+		
+		Bitmap bitmapple = BitmapFactory.decodeResource(getResources(), R.drawable.apple);
+		ImageView appleView =  new ImageView(this);
+		appleView.setImageBitmap(bitmapple);
+		Object[] info = {"apples grow on trees", "counter", bitmapple };
+		Item.infoMap.put("apple",info);
+		
+		Bitmap bitmapotato = BitmapFactory.decodeResource(getResources(), R.drawable.potato);
+		ImageView potatoView = new ImageView(this);
+		potatoView.setImageBitmap(bitmapotato);		
+		Object[] info2 = {"potatoes grow in the earth", "counter", bitmapotato};
+		Item.infoMap.put("potato",info2);
+		
+		Bitmap bitmapberry = BitmapFactory.decodeResource(getResources(), R.drawable.strawberry);
+		ImageView strawberryView = new ImageView(this);
+		strawberryView.setImageBitmap(bitmapberry);		
+		Object[] info3 = {"strawberries are good", "refrigerator", bitmapberry};
+		Item.infoMap.put("strawberries", info3);
+	}
+
 
 }
